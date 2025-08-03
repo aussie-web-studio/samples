2c173cd15124b8a2bd970dcd3e5e8b2c2a3b4376
Andy <andy.phamduccuong@gmail.com>
2025-08-04 07:59:06 +0930

    Play with Arize observability
    Using OpenAI model, integrate with Arize platform

--- 01-tutorials/01-fundamentals/09-simple-observability/recipe_bot.py
+++ 01-tutorials/01-fundamentals/09-simple-observability/recipe_bot.py
@@ -14,6 +14,18 @@ logging.getLogger("strands").setLevel(
     logging.INFO
 )  # Set to DEBUG for more detailed logs
 
+# Configure OpenTelemetry for observability
+#from opentelemetry.sdk.trace.export import BatchSpanProcessor
+#from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+#from strands_to_openinference_mapping import StrandsToOpenInferenceProcessor
+
+from arize.otel import register
+from openinference.instrumentation.openai import OpenAIInstrumentor
+
+
+#from opentelemetry import trace
+#import grpc
+
 
 # Define a websearch tool
 @tool
@@ -47,6 +59,17 @@ from strands.models.openai import OpenAIModel
 # Rertieve OpenAI API key from environment variables
 
 openai_api_key = os.getenv("OpenAI_API_KEY")
+SPACE_ID = os.environ["ARIZE_SPACE_ID"]
+API_KEY = os.environ["ARIZE_API_KEY"]
+ENDPOINT = "otlp.arize.com:443"
+
+tracer_provider = register(
+    space_id=SPACE_ID,
+    api_key=API_KEY,
+    project_name="strands-agent-integration",
+)
+
+OpenAIInstrumentor().instrument(tracer_provider=tracer_provider)
 
 model = OpenAIModel(
     client_args={
@@ -65,7 +88,17 @@ recipe_agent = Agent(
     system_prompt="""You are RecipeBot, a helpful cooking assistant.
     Help users find recipes based on ingredients and answer cooking questions.
     Use the websearch tool to find recipes when users mention ingredients or to look up cooking information.""",
-    tools=[websearch],model=model
+    tools=[websearch],
+    model=model,
+    trace_attributes={
+        "session.id": "abc-1234",
+        "user.id": "andypham",
+        "arize.tags": [
+            "Agent-SDK",
+            "Arize-Project",
+            "OpenInference-Integration",
+        ]
+    }
 )
 
 
